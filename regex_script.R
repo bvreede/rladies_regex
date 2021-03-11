0627dac12afc696c83c5a0b5d861950d1fb70f1d
Barbara Vreede <b.vreede@gmail.com>
2021-03-11 18:45:43 +0100

    clean up script

--- regex_script.R
+++ regex_script.R
@@ -3,32 +3,33 @@ library(RCurl)
 library(tidyverse)
 library(openxlsx)
 
+
+# open the URL and read the page
 url<-"https://www.waterkwaliteitsportaal.nl/WKP.WebApplication/Beheer/Data/GrondwaterkwaliteitMeetgegevens"
-
 page<-readLines(url)
-#page   <- getURL(url)
 parsed <- htmlParse(page)
-links  <- paste("https://www.waterkwaliteitsportaal.nl", xpathSApply(parsed, path="//a", xmlGetAttr, "href"),sep="")
-inds   <- grep("IMmetingen_GWkwaliteit_NL", links)
-links  <- links[inds]
 
+# point links to the correct absolute domain
+links  <- paste("https://www.waterkwaliteitsportaal.nl", 
+                xpathSApply(parsed, path="//a", xmlGetAttr, "href"),
+                sep="")
+
+# select relevant links
+links   <- grep("IMmetingen_GWkwaliteit_NL", links, value=T)
+
+# determine destination files
 regex_match <- regexpr("[^/]+$", links, perl=TRUE)
 destination <- regmatches(links, regex_match)
-#setInternet2(use = TRUE)
 
-
+# download the files from links and unzip
 for(i in seq_along(links)){
   download.file(links[i], destfile=destination[i],method="libcurl")
   Sys.sleep(runif(1, 1, 5))
 }
-
-# unzip the zipped files in the folder
 lapply(destination,unzip)
 
-#IMmetingen_GWkwaliteit_NL67_20191210.csv
+# identify data files, read and collect the data
 ll<-list.files(pattern = "NL[0-9]{2}_[0-9]{8}\\.csv$")
-
-
 dataset <- do.call("rbind", lapply(ll, FUN = function(file) {
   read.csv2(file)
 }))
